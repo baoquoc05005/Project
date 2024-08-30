56777e980b5251e5d4db9b437e5fc42f058b958a
quocbao050405 <baotran05042005@gmail.com>
2024-08-30 14:08:42 -0400

    SpeedTyping File Structure

---
+++ SpeedTyping Game/index.js
@@ -0,0 +1,19 @@
+let API_URL = "https://random-word-api.herokuapp.com/word";
+
+function getQuoteAPI() {
+    return fetch(API_URL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+getQuoteAPI();
