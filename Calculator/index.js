bed992b343d91de91e0497995e56f2073cd96e7a
quocbao050405 <baotran05042005@gmail.com>
2024-08-31 18:01:39 -0400

    Display result on Browser(BETA)

--- Calculator/index.js
+++ Calculator/index.js
@@ -9,36 +9,42 @@ function computerMove(){
         return compMove = "Scissor";
     };
 };
-let playerMove = document.querySelector("button").value;
-function comPare(){
+function comPare(playerMove){
     let compMove = computerMove();
+    let display = document.querySelector("#display");
     if(compMove === "Rock"){
         if(playerMove === "Rock"){
-            console.log("Tie");
+            display.textContent = "Tie";
         }
         else if(playerMove === "Paper"){
-            console.log("You Win!");
-        } else {console.log("You Lose!")};
+            display.textContent = "You Win!";
+        } else {display.textContent = "You Lose!"};
 
     } else if(compMove === "Paper"){
         if(playerMove === "Rock"){
-            console.log("You Lose!");
+            display.textContent = "You Lose!";
         }
         else if(playerMove === "Paper"){
-            console.log("Tie");
-        } else {console.log("You Win!")};
+            display.textContent = "Tie";
+        } else {display.textContent = "You Win!"};
 
     } else if(compMove === "Scissor"){
         if(playerMove === "Rock"){
-            console.log("You Win!");
+            display.textContent = "You Win!";
         }
         else if(playerMove === "Paper"){
-            console.log("You Lose!");
-        } else {console.log("Tie")};
+            display.textContent = "You Lose!";
+        } else {display.textContent = "Tie"};
 
     }
+
+    return display.textContent
 }
 
-document.querySelectorAll("button").forEach(element => {
-    
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        let playerMove = this.value;
+        comPare(playerMove);
+
+    });
 });
